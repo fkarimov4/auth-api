fdd1ef8a9d245493643e7c5c288018bb21e4aaca
Farrukh Karimov <fkarimov4@gmail.com>
2022-03-01 09:51:43 -0500

    added get users route

--- src/users.js
+++ src/users.js
@@ -87,3 +87,30 @@ exports.loginUser = (req, res) => {
       });
     });
 };
+
+exports.getUsers = (req, res) => {
+  //TODO: Protect this route with JWT
+  const db = connectDb();
+  db.collection("users")
+    .get()
+    .then((snapshot) => {
+      const users = snapshot.docs.map((doc) => {
+        let user = doc.data();
+        user.id = doc.id;
+        user.password = undefined;
+        return user;
+      });
+      res.send({
+          success: true,
+          message: "Users returned",
+          users
+      })
+    })
+    .catch((err) => {
+      res.status(500).send({
+        success: false,
+        message: err.message,
+        error: err,
+      });
+    });
+};
